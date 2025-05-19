39833a64d7a49c7c7af456678b5ffe2b6d800b7c
AstralGuardian <72z06x07@gmail.com>
2025-05-19 16:29:41 +0800

    Debug and Merge

---
+++ Tools/MediaProcess.py
@@ -0,0 +1,194 @@
+from pathlib import Path
+from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
+
+class FlacProcess:
+    VALID_SAMPLE_RATES = {22050, 32000, 44100, 48000}
+    VALID_BITRATES = {"32k", "64k", "96k", "128k", "192k", "256k", "320k"}
+
+    def __init__(
+        self,
+        input_folder="OriginalVoice",
+        output_directory=None,
+        sample_rate=32000,
+        bitrate="256k",
+        channels=1,
+        silence_threshold=-50,
+        min_silence_duration=500,
+        padding_duration=300
+    ):
+        self.script_dir = Path(__file__).parent
+        self.input_folder = input_folder
+        self.input_dir = self.script_dir / self.input_folder
+        self.output_dir = self.input_dir.parent if output_directory is None else Path(output_directory)
+        self.output_dir_trimmed = self.output_dir / "Trimmed_audio"
+        self.output_dir_compressed = self.output_dir / "Compressed_audio"
+        self.output_dir_finalname = self.output_dir / "media"
+        self.sample_rate = sample_rate
+        self.bitrate = bitrate
+        self.channels = channels
+        self.silence_threshold = silence_threshold
+        self.min_silence_duration = min_silence_duration
+        self.padding_duration = padding_duration
+
+        if not self.input_dir.exists():
+            raise FileNotFoundError(f"Input directory {self.input_dir} does not exist.")
+
+
+    def count_flac_files(self):
+        '''count the number of FLAC files in the input directory'''
+        return len(list(self.input_dir.glob("*.flac")))
+        
+
+    def strip_silence(self, audio_segment):
+        '''remove silence of the beginning and end of an audio segment'''
+        non_silence_sections = detect_nonsilent(
+            audio_segment,
+            min_silence_len=self.min_silence_duration,
+            silence_thresh=self.silence_threshold
+        )
+        if not non_silence_sections:
+            print("No no-silent segments detected.")
+            return audio_segment
+        
+        start_time, end_time = non_silence_sections[0][0], non_silence_sections[-1][1]
+        extended_end = min(end_time + self.padding_duration, len(audio_segment))
+        return audio_segment[start_time:extended_end]
+        
+
+    def batch_strip_silence(self):
+        '''Process all FALC files in the input directory to remove silence.'''
+        flac_files = list(self.input_dir.glob("*.flac"))
+        if not flac_files:
+            print(f"No flac files found in {self.input_dir}")
+            exit()
+        self.output_dir_trimmed.mkdir(parents=True, exist_ok=True)
+        
+        for flac_file in flac_files:
+            try:
+                audio = AudioSegment.from_file(flac_file, format="flac")
+                trimmed_audio = self.strip_silence(audio)
+                output_path = self.output_dir_trimmed /flac_file.name
+                trimmed_audio.export(output_path, format="flac")
+                print(f"Processed silence removal: {flac_file.name} -> {output_path.name}")
+            except Exception as e:
+                print(f"Error processing {flac_file.name}: {str(e)}")
+
+
+    def batch_compress_to_mp3(self):
+        '''Compress or convert FLAC files to MP3 format.'''
+        flac_files = list(self.output_dir_trimmed.glob("*.flac"))
+        if not self.output_dir_trimmed.exists() or not flac_files:
+            print(f"No trimmed FLAC files found in {self.output_dir_trimmed}. Please run silence_removal first ")
+            return 
+        self.output_dir_compressed.mkdir(parents=True, exist_ok=True)
+        
+        for flac_file in flac_files:
+            try:
+                audio = AudioSegment.from_file(flac_file, format="flac")
+                processed_audio = audio.set_frame_rate(self.sample_rate).set_channels(self.channels)
+                output_path = self.output_dir_compressed / f"{flac_file.stem}.mp3"
+                processed_audio.export(output_path, format="mp3", bitrate=self.bitrate)
+                print(f"Converted: {flac_file.name} -> {output_path.name}")
+            except Exception as e:
+                print(f"Error converting {flac_file.name}: {str(e)}")
+
+
+    def trim_and_convert_flac(self):
+        flac_files = list(self.input_dir.glob("*.flac"))
+        if not flac_files:
+            print(f"No FLAC file found in {self.input_dir}")
+            return
+        self.output_dir_finalname.mkdir(parents=True, exist_ok=True)
+        
+        for flac_file in flac_files:
+            try:
+                audio = AudioSegment.from_file(flac_file, format="flac")
+                trim_audio = self.strip_silence(audio)
+                processed_audio = trim_audio.set_frame_rate(self.sample_rate).set_channels(self.channels)
+                output_path = self.output_dir_finalname / f"{flac_file.stem}.mp3"
+                processed_audio.export(output_path, format="mp3", bitrate=self.bitrate)
+                print(f"Trim & Compress Convert: {flac_file.name} -> {output_path.name}")
+            except Exception as e:
+                print(f"Error Compressing and converting {flac_file}: {str(e)}")
+
+
+class Batch_Modify_Filename:
+    def __init__(self):
+        self.script_dir = Path(__file__).parent
+        self.input_dir = self.script_dir / "media"
+
+        if not self.input_dir.exists():
+            print(f"Input directory {self.input_dir} does not exist.")
+            exit()
+
+    def add_prefix(self, prefix):
+        media_files = list(self.input_dir.glob("*.mp3")) + list(self.input_dir.glob("*.png"))
+        if not media_files:
+            print(f"No mp3 and png files in {self.input_dir}")
+            return
+        
+        for file in media_files:
+            new_name = f"{prefix}{file.name}"
+            output_path = file.with_name(new_name)
+            file.rename(output_path)
+            print(f"Add prefix: {file.name} -> {new_name}")
+
+    def delete_prefix(self, prefix):
+        media_files = list(self.input_dir.glob(f"{prefix}*.mp3")) + list(self.input_dir.glob(f"{prefix}*.png"))
+        if not media_files:
+            print(f"No mp3 and png files with '{prefix}' in {self.input_dir}")
+            return
+        
+        for file in media_files:
+            if file.name.startswith(f"{prefix}"):
+                new_name = file.name[len(prefix):]
+                output_path = file.with_name(new_name)
+                file.rename(output_path)
+                print(f"Delete Prefix: {file.name} -> {new_name}")
+
+
+
+
+if __name__ == "__main__":
+
+    flacprocess = FlacProcess(
+        input_folder= "OriginalVoice",
+        sample_rate= 32000,
+        bitrate= "128k",
+        channels= 1,
+        silence_threshold= -50,
+        min_silence_duration= 600,
+        padding_duration= 300
+    )
+    
+    filename = Batch_Modify_Filename()
+
+    flac_count = flacprocess.count_flac_files()
+    print(f"\nfind {flac_count} .flac files in {flacprocess.input_dir}")
+    
+    
+    print("\nAvailable functions:")
+    print("1. Remove silence of the beginning and end")
+    print("2. Compress and Convert FLAC files to MP3")
+    print("3. Trim & Compress Convert FLAC files")
+    print("4. Add prefix")
+    print("5. Delete prefix")
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        flacprocess.batch_strip_silence()
+    elif choice == "2":
+        flacprocess.batch_compress_to_mp3()
+    elif choice == "3":
+        flacprocess.trim_and_convert_flac()
+    elif choice == "4":
+        filename.add_prefix(input("Input prefix: "))
+    elif choice == "5":
+        filename.delete_prefix(input("Input prefix: "))
+    else:
+        print("Invalid Choice.")
+
+
+
+
--- Tools/mdFileGenerate.py
+++ Tools/mdFileGenerate.py
@@ -1,39 +1,27 @@
 from pathlib import Path
+import re
 
-class AnkiCardGenerator:
+class MdFileGenerate:
+    
     def __init__(
-        self,
-        input_folder,
-        output_directory=None,
-    ):
+        self, 
+        input_folder = "media",
+
+        prefix = ""
+        ):
+        self.script_dir = Path(__file__).parent
         self.input_folder = input_folder
-        self.script_path = Path(__file__).resolve()
-        self.input_dir = self.script_path.parent / input_folder
-        self.output_dir = self.input_dir.parent if output_directory is None else Path(output_directory)
-        self.tags_name = self.input_dir.name.replace(" ", "")
+        self.input_dir = self.script_dir / self.input_folder
+        self.attachment_dir = self.script_dir  / "attachments"
+        self.prefix = prefix
+        self.character_name = self.script_dir.name.replace(" ", "")
 
         if not self.input_dir.exists():
             raise FileNotFoundError(f"Input directory {self.input_dir} does not exist.")
-        
-    def generate_cards(self):
-        audio_files = {f.stem: f for f in self.input_dir.glob("*.mp3")}
-        image_files = {f.stem: f for f in self.input_dir.glob("*.png")}
-        all_files = set(audio_files.keys()).union(set(image_files.keys()))
-
-        self.output_mdfile_path = self.output_dir / f"{self.input_dir.name}.md"
-        with open(self.output_mdfile_path, "w", encoding='utf-8') as f:
-            self.write_header(f)
-            for file_stem in sorted(all_files):
-                audio = audio_files.get(file_stem)
-                image = image_files.get(file_stem)
-                self.write_card(f,audio,image)
-                print(f"Created card for: {file_stem}")
-        print(f"Cards generated successfully in: {generator.output_mdfile_path}")
-            
 
     def write_header(self, file):
         '''Write header'''
-        file.write("TARGET DECK: Genshin\n")
+        file.write(f"TARGET DECK: Genshin::{self.character_name}\n")
 
     def write_card(self, file, audio_file, image_file):
         '''Write a single card'''
@@ -49,26 +37,197 @@ class AnkiCardGenerator:
             file.write(f"![[{image_path}]]\n\n")
 
         file.write("Back:\n\n\n")
-        file.write(f"Tags: Genshin {self.tags_name}\n")
+        file.write(f"Tags: Genshin::{self.character_name}\n")
         file.write("END")
+
+    def generate_cards(self):
+        audio_files = {f.stem: f for f in self.input_dir.glob("*.mp3")}
+        image_files = {f.stem: f for f in self.input_dir.glob("*.png")}
+        all_files = set(audio_files.keys()).union(set(image_files.keys()))
+
+        output_path = self.input_dir.parent / f"{self.script_dir.name}.md"
+        with open(output_path, "w", encoding='utf-8') as f:
+            self.write_header(f)
+            for file_stem in sorted(all_files):
+                audio = audio_files.get(file_stem)
+                image = image_files.get(file_stem)
+                self.write_card(f,audio,image)
+                print(f"Created card for: {file_stem}")
+        print(f"Cards generated successfully in: {output_path}")
+
+    def count_cards(self,file_path):
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                count = 0
+                for line in lines:
+                    if line.startswith('END'):
+                        count = count + 1
+        except Exception as e:
+            print(f"Error in count_cards: {str(e)}")
+        return count
+                
+
+    def find_markdown_files(self) -> list[Path]:
+        '''return a list of all markdown files in the working directory'''
+        return list(self.script_dir.glob("*.md"))
+
+
+    def add_delete(self, mdfile_path: Path):
+        '''Add 'DELETE' line before each line starting with '<!--ID'.'''
+        try:
+            with open(mdfile_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+            new_lines = []
+            for line in lines:
+                if line.strip().startswith('<!--ID'):
+                    new_lines.append("DELETE\n")
+                new_lines.append(line)
+            with open(mdfile_path, 'w', encoding='utf-8') as file:
+                file.writelines(new_lines)
+        except Exception as e:
+            print(f"Error in adding delete: {str(e)}")
+        
+
+    def deleteIDs(self, mdfile_path: Path):
+        '''Delete each "<!--ID: -->" Line '''
+        try:
+            with open(mdfile_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+            new_lines = []
+            for line in lines:
+                if not re.match(r'^\s*<!--ID:.*?-->\s*$', line):
+                    new_lines.append(line)
+            with open(mdfile_path, 'w', encoding='utf-8') as file:
+                file.writelines(new_lines)
+        except Exception as e:
+            print(f"Error in deleteIDs: {str(e)}")
+
+
+    def select_file(self) -> Path:
+        '''Display files with numbers and let user select one'''
+        print("Available markdown files: ")
+        files = self.find_markdown_files()
+        for i, file in enumerate(files, 1):
+            print(f"{i}. {file.name}")
+
+        try:
+            choice = int(input(f"Enter the number of the files to process (1-{len(files)}): "))
+            if 1<= choice <= len(files):
+                return files[choice-1]
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+    def update_media_path(self, mdfile_path:Path):
+        '''Update media path in markdown file'''
+        audio_files = list(self.input_dir.glob("*.mp3"))
+        image_files = list(self.input_dir.glob("*.png"))
+#        pasted_image =  list(self.attachment_dir.glob("*.png"))
+        new_lines = []
+        try:
+            with open(mdfile_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+            for line in lines:
+                line_stripped = line.strip()
+                
+                if re.match(r'^!\[\[attachments/.*?\]\]$', line_stripped):
+                    new_lines.append(line)
+        
+                elif re.match(r'^!\[\[.*?\]\]$', line_stripped):
+                    media_name = line_stripped[3:-2].split('/')[-1].split('|')[0]
+
+                    for audio_file in audio_files:
+                        if audio_file.name[:len(self.prefix)] == self.prefix:
+                            if media_name == audio_file.name[len(self.prefix):]:
+                                audio_relative_path = f"{self.input_folder}/{audio_file.name}"
+                                new_lines.append(f"![[{audio_relative_path}]]\n")
+                        elif media_name[:len(self.prefix)] == self.prefix:
+                            if media_name[len(self.prefix):] == audio_file.name:
+                                audio_relative_path = f"{self.input_folder}/{audio_file.name}"
+                                new_lines.append(f"![[{audio_relative_path}]]\n")
+                        elif media_name == audio_file.name:
+                            audio_relative_path = f"{self.input_folder}/{audio_file.name}"
+                            new_lines.append(f"![[{audio_relative_path}]]\n")
+                            
+                    for image_file in image_files:
+                        if image_file.name[:len(self.prefix)] == self.prefix:
+                            if media_name in image_file.name[len(self.prefix):]:
+                                image_relative_path = f"{self.input_folder}/{image_file.name}"
+                                new_lines.append(f"![[{image_relative_path}]]\n")
+                        elif media_name[:len(self.prefix)] == self.prefix:
+                            if media_name[len(self.prefix):] in image_file.name:
+                                image_relative_path = f"{self.input_folder}/{image_file.name}"
+                                new_lines.append(f"![[{image_relative_path}]]\n")
+                        elif media_name == image_file.name:
+                            image_relative_path = f"{self.input_folder}/{image_file.name}"
+                            new_lines.append(f"![[{image_relative_path}]]\n")
+                else:
+                    new_lines.append(line)
+                    
+            output_path = mdfile_path.parent / f"{mdfile_path.stem}.md" 
+            with open(output_path, 'w', encoding='utf-8') as file:
+                file.writelines(new_lines)
+        except Exception as e:
+            print(f"Error in update media path: {str(e)}")
+
+    
+    def update_tags(self, mdfile_path:Path):
+        try:
+            with open(mdfile_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                new_lines = []
+                for line in lines:
+                    line_stripped = line.strip()
+                    if line_stripped.startswith("Tags: "):
+                        parts = line_stripped.split(' ')
+                        new_tags = "Tags: " + parts[1] + "::" + parts[2] + "\n"
+                        new_lines.append(new_tags)
+                    else:
+                        new_lines.append(line)
+
+            output_path = mdfile_path.parent / f"{mdfile_path.stem}.md"
+            with open(output_path, 'w', encoding='utf-8') as file:
+                file.writelines(new_lines)
+        except Exception as e:
+            print(f"Error in update_tags: {str(e)}")
+
+
+
+
+class ui:
+    def __init__(self):
+        pass
 
 
 
 if __name__ == "__main__":
-    folder_name = input("Enter folder name: ")
-    generator = AnkiCardGenerator(folder_name)
-    generator.generate_cards()
-    
+    processor = MdFileGenerate(prefix="Furina_")
+    markdown_files = processor.find_markdown_files()
+
+    print("1. Count Cards")
+    print("2. Delete IDs")
+    print("3. Update media path")
+    print("4. Update Tags")
+    print("5. Generate cards")
+    Function_selection = input("\nSelect a function(1-5): ")
 
 
-
-
-
-
-
-
-
-
+    if Function_selection == "1":
+        selected_file = processor.select_file()
+        num = processor.count_cards(selected_file)
+        print(num)
+    elif Function_selection == "2":
+        selected_file = processor.select_file()
+        processor.deleteIDs(selected_file)
+    elif Function_selection == "3":
+        selected_file = processor.select_file()
+        processor.update_media_path(selected_file)
+    elif Function_selection == "4":
+        selected_file = processor.select_file()
+        processor.update_tags(selected_file)
+    elif Function_selection == "5":
+        processor.generate_cards()
 
 
 
